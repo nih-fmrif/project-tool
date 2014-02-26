196046afff35164f9fb393c75498bb34f30fd80b
Joseph Naegele <joseph.naegele@gmail.com>
2014-02-26 10:22:04 -0500

    `project list` shows your projects only by default

--- project_manager.py
+++ project_manager.py
@@ -62,9 +62,11 @@ def main():
     parent_parser.add_argument("project", help="name of project")
 
     list_parser = subparsers.add_parser("list",
-            help="list all projects",
-            epilog="Lists all projects found in PROJECT_ROOT",
+            help="list projects",
+            epilog="Lists all projects to which you have access",
             formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    list_parser.add_argument("-a", "--all", action="store_true",
+            help="list ALL projects in PROJECT_ROOT")
     list_parser.set_defaults(func=list_projects)
 
     create_parser = subparsers.add_parser("create",
@@ -151,10 +153,12 @@ def main():
 
     # determine username of user running this program (Unix)
     args.executer = pwd.getpwuid(os.getuid()).pw_name
+    logging.debug("You are: %s" % args.executer)
 
     if not os.path.isdir(args.project_root):
         logging.error("Project root %s is not a directory." % args.project_root)
     PROJECT_ROOT = args.project_root
+    logging.info("PROJECT_ROOT: %s" % PROJECT_ROOT)
 
     args.func(args)
 
@@ -164,13 +168,31 @@ def fail(msg):
     sys.exit(1)
 
 def list_projects(args):
-    projs = []
+    projects = []
     for dirname in os.listdir(PROJECT_ROOT):
         pdir = os.path.join(PROJECT_ROOT, dirname)
         pconf = project_conf_path(dirname)
-        if os.path.isdir(pdir) and os.path.isfile(pconf):
-            projs.append(dirname)
-    for proj in projs:
+        if not os.path.isdir(pdir):
+            logging.debug("Unknown project: %s" % pdir)
+            continue
+        if not os.path.isfile(pconf):
+            logging.debug("Unknown config file: %s" % pconf)
+            continue
+        projname = dirname
+        if args.all:
+            projects.append(projname)
+        else:
+            # only append projects for which user has access
+            try:
+                conf = read_project_conf_path(projname)
+            except IOError:
+                continue    # no permission to read YAML
+            users = [conf.owner] + conf.members + conf.collaborators
+            for username in users:
+                if args.executer == username:
+                    projects.append(projname)
+
+    for proj in projects:
         print(' ' * 4 + proj)
 
 def create_project(args):
@@ -181,13 +203,14 @@ def create_project(args):
     if os.path.isfile(pconf):
         fail("Project config '%s' already exists" % pconf)
 
+    logging.info("Creating directory: %s" % pdir)
     os.mkdir(pdir)
-    os.chown(pdir, os.getuid(), os.getgid())
-
+    logging.info("Creating config file: %s" % pconf)
     conf = ProjectDB(args.project, args.executer, args.public)
+
+    # this is the only place where we save the project config BEFORE updating permissions
     write_project_conf_path(conf)
     update_perms(conf)
-    os.chown(pconf, os.getuid(), os.getgid())
 
 def delete_project(args):
     check_project_exists(args.project)
@@ -196,7 +219,9 @@ def delete_project(args):
     if conf.owner != args.executer:
         fail("Only the project owner can delete a project")
 
+    logging.debug("Removing project directory")
     shutil.rmtree(project_dir_path(args.project))
+    logging.debug("Removing project config file")
     os.remove(project_conf_path(args.project))
 
 def print_info(args):
@@ -208,21 +233,29 @@ def refresh_permissions(args):
     check_project_exists(args.project)
     conf = read_project_conf_path(args.project)
 
-    if (args.executer not in conf.members and args.executer not in conf.collaborators
-            and args.executer != conf.owner):
-        fail("Only a project member may update the project's permissions")
-
-    update_perms(conf)
+    if args.executer in conf.members or args.executer == conf.owner:
+        update_perms(conf)
+    else:
+        fail("Only a project owner/member may update its permissions")
 
 def update_perms(conf):
     pdir = project_dir_path(conf.project)
+    pconf = project_conf_path(conf.project)
+
+    logging.info("Chown project directory: %s" % pdir)
+    os.chown(pdir, os.getuid(), os.getgid())
+    logging.info("Recursively updating ACL on project directory")
     set_access(pdir, conf.owner, conf.members, conf.collaborators, conf.public)
-    set_access(project_conf_path(conf.project), conf.owner, [], [], conf.public)
+
+    logging.info("Chown project config file: %s" % pconf)
+    os.chown(pconf, os.getuid(), os.getgid())
+    logging.info("Updating ACL on project config file")
+    set_access(pconf, conf.owner, [], [], conf.public)
 
 def is_subdir(path, subdir):
+    """Tested in 'test_project_manager.py'"""
     path = os.path.realpath(os.path.expanduser(path))
     subdir = os.path.realpath(os.path.expanduser(subdir))
-
     if subdir == path:
         return True
     elif subdir == '/':
@@ -236,13 +269,9 @@ def set_access(root, owner, read_write, read_only, public):
     Clear and reset the ACL for each file/directory in the project.
     Recursively change the owner of all files to the project's owner.
     """
-
     # Don't allow top-level files/dirs to be symbolic links
     if os.path.islink(root):
         fail("%s is a symbolic link. Cannot update ACL")
-
-    # Owner always has read/write permissions
-    read_write.insert(0, owner)
 
     acl_text = 'u::rwx,g::rwx'
     if public:
@@ -250,8 +279,11 @@ def set_access(root, owner, read_write, read_only, public):
     else:
         acl_text += ',o::---'
 
-    for user in read_write:
+    # Owners and Members have read/write
+    writers = read_write + [owner]
+    for user in writers:
         acl_text += ',u:%s:rwx' % user
+    # Collaborators have read-only
     for user in read_only:
         acl_text += ',u:%s:r-x' % user
 
@@ -259,6 +291,7 @@ def set_access(root, owner, read_write, read_only, public):
     acl.calc_mask()
 
     if not acl.valid():
+        logging.debug("Bad ACL: %s" % acl_text)
         fail("Error generating ACL. Please notify system administrator.")
 
     apply_acl(root, acl)
@@ -270,11 +303,12 @@ def set_access(root, owner, read_write, read_only, public):
             if is_subdir(PROJECT_ROOT, realpath):
                 apply_acl(path, acl)
             else:
-                fail("%s is actually %s,\n"
-                        "which is outside PROJECT_ROOT (%s). "
-                        "Cannot continue." % (path, realpath, PROJECT_ROOT))
+                logging.warning(
+                        "%s is actually %s, which is not in $PROJECT_ROOT" %
+                        (path, realpath))
 
 def apply_acl(path, acl):
+    logging.debug("Applying ACL to %s" % path)
     if os.path.isdir(path):
         try:
             posix1e.delete_default(path)
@@ -293,6 +327,7 @@ def mod_user(args):
     check_project_exists(args.project)
 
     try:
+        # "Look up" user
         pwd.getpwnam(args.username)
     except KeyError:
         fail("User %s is not a valid user" % args.username)
@@ -307,31 +342,40 @@ def mod_user(args):
             fail("Can't remove permissions from owner. Set a new owner first")
         else:
             fail("Already project owner")
-    if args.username in conf.members:
+    elif args.username in conf.members:
         if args.role != MEMBER_ROLE:
+            logging.info("Removing %s from members" % args.username)
             conf.members.remove(args.username)
         else:
             fail("Already a member")
-    if args.username in conf.collaborators:
+    elif args.username in conf.collaborators:
         if args.role != COLLAB_ROLE:
+            logging.info("Removing %s from collaborators" % args.username)
             conf.collaborators.remove(args.username)
         else:
             fail("Already a collaborator")
 
     if args.role == "owner":
+        prev_owner = conf.owner
+        logging.info("Setting %s as new owner" % args.username)
         conf.owner = args.username
+        logging.info("Demoting previous owner %s" % prev_owner)
+        conf.members.append(prev_owner)
     elif args.role == MEMBER_ROLE:
+        logging.info("Setting %s as member" % args.username)
         conf.members.append(args.username)
     elif args.role == COLLAB_ROLE:
+        logging.info("Setting %s as collaborator" % args.username)
         conf.collaborators.append(args.username)
 
-    write_project_conf_path(conf)
     update_perms(conf)
+    write_project_conf_path(conf)
 
 def del_user(args):
     check_project_exists(args.project)
 
     try:
+        # "Look up" user
         pwd.getpwnam(args.username)
     except KeyError:
         fail("User %s is not a valid user" % args.username)
@@ -344,12 +388,14 @@ def del_user(args):
     if args.username == conf.owner:
         fail("Can't delete owner. Set a new owner first")
     if args.username in conf.members:
+        logging.info("Removing %s from members" % args.username)
         conf.members.remove(args.username)
     if args.username in conf.collaborators:
+        logging.info("Removing %s from members" % args.username)
         conf.collaborators.remove(args.username)
 
-    write_project_conf_path(conf)
     update_perms(conf)
+    write_project_conf_path(conf)
 
 def check_project_exists(project_name):
     d = project_dir_path(project_name)
